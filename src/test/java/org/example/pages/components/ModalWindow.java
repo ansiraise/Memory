15c8eff5c7b9fcf144baa09216a6c276ac6950df
Alex <283191474+ansiraise@users.noreply.github.com>
2026-05-11 13:07:20 +0300

    Version v2.1 (minor fixies)

--- src/test/java/org/example/pages/components/ModalWindow.java
+++ src/test/java/org/example/pages/components/ModalWindow.java
@@ -3,31 +3,33 @@ package org.example.pages.components;
 import org.example.DriverSingleton;
 import org.example.pages.CartPage;
 import org.example.pages.LoginPage;
+import org.example.utils.WaitHelper;
 import org.openqa.selenium.By;
 
 public class ModalWindow {
 
-    private By modalContinueShopBtn = By.xpath("//div[@class='modal-content']//button[text()='Continue Shopping']");
-    private By modalViewCartBtn = By.xpath("//div[@class='modal-content']//a[@href='/view_cart']");
-    private By continueOnCart = By.xpath("//div[@class='modal-content']//button[text()='Continue On Cart']");
-    private By registerLoginBtn = By.xpath("//div[@class='modal-content']//a[@href='/login']");
+    private final By modalContinueShopBtnLocator = By.xpath("//div[@class='modal-content']//button[text()='Continue Shopping']");
+    private final By modalViewCartBtnLocator = By.xpath("//div[@class='modal-content']//a[@href='/view_cart']");
+    private final By continueOnCartLocator = By.xpath("//div[@class='modal-content']//button[text()='Continue On Cart']");
+    private final By registerLoginBtnLocator = By.xpath("//div[@class='modal-content']//a[@href='/login']");
 
 
     public void clickContinueShopping() {
-        DriverSingleton.getDriver().findElement(modalContinueShopBtn).click();
+        WaitHelper.waitForElementVisible(modalContinueShopBtnLocator);
+        DriverSingleton.getDriver().findElement(modalContinueShopBtnLocator).click();
     }
 
     public CartPage clickViewCartBtn() {
-        DriverSingleton.getDriver().findElement(modalViewCartBtn).click();
+        DriverSingleton.getDriver().findElement(modalViewCartBtnLocator).click();
         return new CartPage();
     }
 
     public void clickContinueOnCartBtn() {
-        DriverSingleton.getDriver().findElement(continueOnCart).click();
+        DriverSingleton.getDriver().findElement(continueOnCartLocator).click();
     }
 
    public LoginPage clickRegisterLoginBtn() {
-        DriverSingleton.getDriver().findElement(registerLoginBtn).click();
+        DriverSingleton.getDriver().findElement(registerLoginBtnLocator).click();
         return new LoginPage();
    }
 
